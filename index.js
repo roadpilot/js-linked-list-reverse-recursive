6241f8bea68424d762f1f731895599bf542a4dfc
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-21 20:28:37 +0000

    problem

---
+++ index.js
@@ -0,0 +1,7 @@
+/** 206 RETURN REVERSE OF SINGLY-LINKED LIST
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
