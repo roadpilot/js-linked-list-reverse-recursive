f0f2a3bc75e0bd1f1fed66a3d1e333af6b5b9fa9
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-21 20:28:55 +0000

    solution

--- index.js
+++ index.js
@@ -5,3 +5,27 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var reverseList = function(head) {
+    if (!head || !head.next) return head
+
+    // console.log(head)
+    let reversedList = reverseList(head.next)
+
+    // console.log("head.val:", head.val, "head.next:", head.next, "reversedList:", reversedList)
+    head.next.next = head
+    head.next = null
+    // console.log(head)
+    
+    return reversedList
+};
+
+/*
+head
+head.val
+head.next
+*/
